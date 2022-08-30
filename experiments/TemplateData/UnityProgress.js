b1e908fdec0282e4b498f2200ef236af7dda532a
ppipelin <ppipelin@users.noreply.github.com>
2022-08-30 19:14:21 +0000

    deploy: 1a13daaabadfb1f300e6362e1c363340e6857477

--- experiments/TemplateData/UnityProgress.js
+++ experiments/TemplateData/UnityProgress.js
@@ -6,7 +6,7 @@ function UnityProgress(unityInstance, progress) {
     unityInstance.logo.className = "logo " + unityInstance.Module.splashScreenStyle;
     unityInstance.container.appendChild(unityInstance.logo);
   }
-  if (!unityInstance.progress) {    
+  if (!unityInstance.progress) {
     unityInstance.progress = document.createElement("div");
     unityInstance.progress.className = "progress " + unityInstance.Module.splashScreenStyle;
     unityInstance.progress.empty = document.createElement("div");
@@ -19,6 +19,6 @@ function UnityProgress(unityInstance, progress) {
   }
   unityInstance.progress.full.style.width = (100 * progress) + "%";
   unityInstance.progress.empty.style.width = (100 * (1 - progress)) + "%";
-  if (progress == 1)
+  if (progress === 1)
     unityInstance.logo.style.display = unityInstance.progress.style.display = "none";
 }
