1adcebfffef9f410df9135abf792be303b9089f8
ppipelin <ppipelin@users.noreply.github.com>
2022-08-30 18:24:26 +0000

    deploy: 01fdde3cb0d0a88144828347e75b38744a37cd0e

--- conf/apache/setup_server.sh
+++ conf/apache/setup_server.sh
@@ -1,6 +1,7 @@
+sudo apt-get update -y && sudo apt-get upgrade
 sudo apt-get install -y apache2 openssl
 sudo cp /etc/apache2/apache2.conf /etc/apache2/apache2.conf.old && sudo cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/000-default.conf.old
-sudo cp ./apache2.conf /etc/apache2/apache2.conf && sudo cp ./sites-available/000-default.conf /etc/apache2/sites-available/000-default.conf # copy local to folder
+sudo cp conf/apache/apache2.conf /etc/apache2/apache2.conf && sudo cp ./conf/apache/sites-available/000-default.conf /etc/apache2/sites-available/000-default.conf # copy local to folder
 sudo a2enmod rewrite
 sudo a2enmod ssl
 sudo service apache2 restart
