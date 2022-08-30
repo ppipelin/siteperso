5e88c8bdaf702e98a0bce8bd1ef40f30a2789ac4
ppipelin <ppipelin@users.noreply.github.com>
2022-08-30 18:44:50 +0000

    deploy: 89096ed807050ce34bf391021a98e384f9158e25

--- conf/apache/setup_server.sh
+++ conf/apache/setup_server.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 sudo apt-get update -y && sudo apt-get upgrade
 sudo apt-get install -y apache2 openssl
 sudo cp /etc/apache2/apache2.conf /etc/apache2/apache2.conf.old && sudo cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/000-default.conf.old
--- conf/merge.sh
+++ conf/merge.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 git fetch --all
 git reset --hard origin/main
 git submodule	update --init --recursive
