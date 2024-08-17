d8d4f07450dd2d605a4f918c29b75adf52395639
ppipelin <ppipelin@users.noreply.github.com>
2024-08-17 11:40:13 +0000

    deploy: ab19772772dbe7bb1cc188272176ea857e36c0ed

--- conf/apache/setup_server.sh
+++ conf/apache/setup_server.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-sudo apt-get update -y && sudo apt-get upgrade
+sudo chown -R www-data:www-data /home/pi/ppipelin.github.io
+sudo chmod -R 777 /home/pi/ppipelin.github.io
+sudo apt-get update -y && sudo apt-get upgrade -y
 sudo apt-get install -y apache2 openssl
 sudo cp /etc/apache2/apache2.conf /etc/apache2/apache2.conf.old && sudo cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/000-default.conf.old
 sudo cp conf/apache/apache2.conf /etc/apache2/apache2.conf && sudo cp ./conf/apache/sites-available/000-default.conf /etc/apache2/sites-available/000-default.conf # copy local to folder
